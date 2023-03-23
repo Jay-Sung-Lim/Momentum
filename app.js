018f338912e5d84c8be4f39af26a3a35f99f3c0d
Jaesung Lim <wotjd2732@gmail.com>
2023-03-23 00:20:35 -0400

    #3.5

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-const title = document.querySelector('div.hello:first-child h1');
+const h1 = document.querySelector('div.hello:first-child h1');
 
 function handleTitleClick() {
-  title.style.color = 'blue';
+  h1.style.color = 'blue';
 }
 
 function handleMouseEnter() {
-  title.innerHTML = 'Mouse is here';
+  h1.innerHTML = 'Mouse is here';
 }
 
 function handleMouseLeave() {
-  title.innerHTML = 'Mouse is gone';
+  h1.innerHTML = 'Mouse is gone';
 }
 
-title.addEventListener('click', handleTitleClick);
-title.addEventListener('mouseenter', handleMouseEnter);
-title.addEventListener('mouseleave', handleMouseLeave);
+function handleWindowResize() {
+  document.body.style.backgroundColor = 'tomato';
+}
+
+function handleWindowCopy() {
+  alert('copy!');
+}
+
+function handleWindowOffline() {
+  alert('no wifi');
+}
+
+function handleWindowOnline() {
+  alert('Wifi On');
+}
+
+h1.onclick = handleTitleClick;
+h1.addEventListener('mouseenter', handleMouseEnter);
+h1.addEventListener('mouseleave', handleMouseLeave);
+
+window.addEventListener('resize', handleWindowResize);
+window.addEventListener('copy', handleWindowCopy);
+window.addEventListener('offline', handleWindowOffline);
+window.addEventListener('online', handleWindowOnline);
