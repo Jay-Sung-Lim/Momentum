12573f3750e8d866413d2e7b3a3e614961daf0dc
Jaesung Lim <wotjd2732@gmail.com>
2023-03-24 00:18:20 -0400

    #3.6 CSS in JS

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
 const h1 = document.querySelector('div.hello:first-child h1');
 
 function handleTitleClick() {
-  h1.style.color = 'blue';
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === 'blue') {
+    newColor = 'tomato';
+  } else {
+    newColor = 'blue';
+  }
+  h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-  h1.innerHTML = 'Mouse is here';
-}
-
-function handleMouseLeave() {
-  h1.innerHTML = 'Mouse is gone';
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = 'tomato';
-}
-
-function handleWindowCopy() {
-  alert('copy!');
-}
-
-function handleWindowOffline() {
-  alert('no wifi');
-}
-
-function handleWindowOnline() {
-  alert('Wifi On');
-}
-
-h1.onclick = handleTitleClick;
-h1.addEventListener('mouseenter', handleMouseEnter);
-h1.addEventListener('mouseleave', handleMouseLeave);
-
-window.addEventListener('resize', handleWindowResize);
-window.addEventListener('copy', handleWindowCopy);
-window.addEventListener('offline', handleWindowOffline);
-window.addEventListener('online', handleWindowOnline);
+h1.addEventListener('click', handleTitleClick);
