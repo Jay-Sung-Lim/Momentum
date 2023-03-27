dc02ebe15aa8e7a5f1e3dd4a2a4472f5e87284e8
Jaesung Lim <wotjd2732@gmail.com>
2023-03-27 03:57:10 -0400

    #4.6 Loading Username

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
+
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
 
 function onLoginBtnSubmit(event) {
   event.preventDefault();
-  const username = loginInput.value;
-  localStorage.setItem('username', username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener('submit', onLoginBtnSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginBtnSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
