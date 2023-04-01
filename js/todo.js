adaed618f4a3d90f79e5c4255c39f0c6380aad62
Jaesung Lim <wotjd2732@gmail.com>
2023-04-01 18:46:18 -0400

    #7.2 Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.querySelector('#todo-form');
 const toDoInput = document.querySelector('#todo-form input');
 const toDoList = document.querySelector('#todo-list');
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement('li');
   const span = document.createElement('span');
+  span.innerText = newTodo;
+
+  const button = document.createElement('button');
+  button.innerText = '❌';
+  button.addEventListener('click', deleteToDo);
   li.appendChild(span);
-  span.innerHTML = newTodo;
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
